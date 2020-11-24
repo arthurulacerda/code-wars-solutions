017abd3a9c97b8daadf36c3250d2cdf164cfca69
Arthur Lacerda <arthur.lacerda@solvimm.com>
2020-11-24 00:17:27 -0300

    refactor tortoise-racing

--- src/problems/tortoise_racing.clj
+++ src/problems/tortoise_racing.clj
@@ -9,17 +9,14 @@
        (> v1 0)
        (> distance 0)))
 
-(defn time-to-reach
-  [v distance]
-  (let [hours-seconds   (/ distance v)
-        hours           (int (/ hours-seconds 3600))
-        minutes-seconds (- hours-seconds (* hours 3600))
-        minutes         (int (/ minutes-seconds 60))
-        seconds         (int (- minutes-seconds (* minutes 60)))]
-    [hours minutes seconds]))
+(defn remainder [t]
+  (* 60 (mod t 1)))
 
 (defn race
   [v1 v2 distance]
   (if (valid? v1 v2 distance)
-    (time-to-reach (/ (- v2 v1) 3600) distance)
+    (let [hour   (float (/ distance (- v2 v1)))
+          minute (remainder hour)
+          second (remainder minute)]
+      (map int [hour minute second]))
     nil))
