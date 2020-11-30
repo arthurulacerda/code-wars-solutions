ab1462c38f6410884c7680419b395b8954ab35b4
Arthur Lacerda <arthur.lacerda@solvimm.com>
2020-11-29 23:53:54 -0300

    simple-encription-one-alternating-split decryption

--- src/problems/simple_encryption_one_alternating_split.clj
+++ src/problems/simple_encryption_one_alternating_split.clj
@@ -3,7 +3,8 @@
 
 (ns problems.simple-encryption-one-alternating-split)
 
-(defn encrypt [st n]
+(defn encrypt
+  [st n]
   (if (> n 0)  
     (recur (str
              (apply str (take-nth 2 (rest st)))
@@ -11,4 +12,15 @@
            (dec n))
     st))
 
-(defn decrypt [st n] st)
+(defn decrypt
+  [st n]
+  (if (> n 0)
+    (let [mid-index  (int (/ (count st) 2))
+          first-half (subs st 0 mid-index)
+          last-half  (subs st mid-index)]
+      (recur (reduce str
+                     (map #(str %2 %1)
+                          (concat first-half (repeat nil))
+                          last-half))
+             (dec n)))
+    st))
